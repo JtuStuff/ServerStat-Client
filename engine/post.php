11db8199821cef32f2e2b51226275c3afe586756
FlyingThaCat <95353217+FlyingThaCat@users.noreply.github.com>
2021-12-13 21:35:11 +0700

    Add files via upload
    Forget to up this...

---
+++ engine/post.php
@@ -0,0 +1,36 @@
+<?php 
+require ('config.php');
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error) {
+    die("Connection failed");
+}
+
+$Status = $conn->real_escape_string($_POST['Status']);
+
+## ---------------|
+## 0449 = No Input|
+## 8690 = Invalid |
+## ---------------|	
+
+if (empty($_POST['Status'])){
+	echo "0449";
+	exit();
+}
+else if (preg_match('/[^a-z_\-0-9]/i', $Status)) {
+	echo "8690";
+	exit();
+}
+
+else {
+
+$sql="INSERT INTO Result (Status, TimeStamp) VALUES ('$Status', CURRENT_TIMESTAMP())";
+
+if (!$conn->query($sql)) {
+    die('Error: ' . mysqli_error($db));
+}
+
+$conn-> close();
+}
+?>
